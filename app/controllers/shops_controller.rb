343632ed8381208c95291ba7889562d65330c230
Zoltan Biber <zoltan.biber@gmail.com>
2014-07-07 14:15:00 +0100

    amended shops' whitelist as per the modified column names

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -12,7 +12,7 @@ class ShopsController < ApplicationController
   def allowed_params #whitelist
     params.require(:shop).permit(:name, :address_city, :address_street, :email, :opening_hours,
     :phone, :website, :address_secondary, :address_building, :address_postcode, :address_state,
-    :address_country, :address_latitude, :address_longitude ) #for each attribute in the model
+    :address_country, :latitude, :longitude ) #for each attribute in the model
   end 
 
 end
