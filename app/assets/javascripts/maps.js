629f0cfb5375b5465d9cc5122679b8d5db7572bc
Zoltan Biber <zoltan.biber@gmail.com>
2014-07-07 21:52:27 +0100

    work in progress / adding markers to map according to lat/lng of shops

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -8,21 +8,34 @@ $(document).ready(function() {
 
   map =  new google.maps.Map($('#map-canvas')[0], options);
 
-function addMarker(lat, lng) {
-  var marker = new google.maps.Marker({
-    position: new google.maps.LatLng(lat, lng),
-    map: map
+  function addMarker(lat, lng) {
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(lat, lng),
+      map: map
+    });
+  }
+
+  var shops = document.getElementById("shop-div");
+
+  _(shops).each(function(shop){
+      
+      console.log(shop.id);
+    
+      var latitude = $("#" + shop.id).data("latitude");
+      var longitude = $("#" + shop.id).data("longitude");
+      addMarker(latitude, longitude);
+
   });
-}
 
+// one approach is to initiate a JSON request 
+// and make the Rails controller respond to JSON
 
-// Initiate a JSON request
-addMarker(51.516614, -0.079358);
-addMarker(55.957175, -3.187894);
-addMarker(51.521371, -0.210583);
-addMarker(51.490694, -0.153452);
-addMarker(51.528913, -0.077664);
+// hardcoded markers
+// addMarker(51.516614, -0.079358);
+// addMarker(55.957175, -3.187894);
+// addMarker(51.521371, -0.210583);
+// addMarker(51.490694, -0.153452);
+// addMarker(51.528913, -0.077664);
 
 });
-
 
