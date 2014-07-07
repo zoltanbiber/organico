27a2948e0ae4efc36d2b424bf1b4c6a5201fb668
Zoltan Biber <zoltan.biber@gmail.com>
2014-07-07 12:15:59 +0100

    specifying what the geocoder input for a shop should be / rspec test passing

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,5 +1,18 @@
 class Shop < ActiveRecord::Base
   has_many :shop_tags
   has_many :tags, :through => :shop_tags
+
+  validates :name, :presence => true
+  validates :address_city, :presence => true
+  validates :address_street, :presence => true
+  validates :address_postcode, :presence => true
+
+  geocoded_by :geocoder_input
+
+  after_validation :geocode
+
+  def geocoder_input
+    "#{address_city}, #{address_street}, #{address_postcode}"
+  end
 end
 
--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -3,6 +3,23 @@ require 'rails_helper'
 RSpec.describe Shop, :type => :model do
   it { should have_many(:shop_tags) }
   it { should have_many(:tags).through(:shop_tags) }
+
+  describe "Geocoding" do
+    before do
+      @shop = Shop.create!(
+        :name => "Planet Organic", 
+        :address_city => "London",
+        :address_street => "10 Devonshire Square",
+        :address_postcode => "EC2M 4AE",
+      )
+    end
+
+    it "should provide useful input to the geocoder" do
+      result = @shop.geocoder_input
+      expect(result).to eq("London, 10 Devonshire Square, EC2M 4AE")
+    end
+
+  end
 end
 
 
