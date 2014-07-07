b646d770cf7328493f09911df3b3e7601dc93934
Zoltan Biber <zoltan.biber@gmail.com>
2014-07-07 10:26:17 +0100

    added hardcoded markers to google maps

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -8,11 +8,18 @@ $(document).ready(function() {
 
   map =  new google.maps.Map($('#map-canvas')[0], options);
 
-});
-
 function addMarker(lat, lng) {
   var marker = new google.maps.Marker({
     position: new google.maps.LatLng(lat, lng),
     map: map
   });
 }
+
+addMarker(51.516614, -0.079358);
+addMarker(55.957175, -3.187894);
+addMarker(51.521371, -0.210583);
+addMarker(51.490694, -0.153452);
+addMarker(51.528913, -0.077664);
+
+});
+
