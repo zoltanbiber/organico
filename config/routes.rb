95cfa8c061aa80d7ff49fb0e1f7d754a7ac9bb7d
Zoltan Biber <zoltan.biber@gmail.com>
2014-07-08 14:21:43 +0100

    added create/show/update routes for Users

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,16 @@ Rails.application.routes.draw do
   devise_for :users
   root 'shops#index'
   resources :shops
+
+  resources :users, :only => [:create, :show, :update] do
+    member do
+      get :show
+      get :update 
+    end
+  end
   
 end
+
+
+
+
