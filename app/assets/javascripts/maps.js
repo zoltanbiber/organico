566c29ca1df05f0aa639e4e42691d44523dd5cf7
Zoltan Biber <zoltan.biber@gmail.com>
2014-07-08 13:43:47 +0100

    clicking on POI centers the map and zooms in

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,6 +1,6 @@
 var map;
 var shops;
-var test;
+
 $(document).ready(function() {
   var options = {
     center: new google.maps.LatLng(51.5, -0.125),
@@ -17,28 +17,7 @@ $(document).ready(function() {
     var longitude = $(shops[i]).data("longitude");
     addMarker(latitude, longitude);
    
-
   }
-
-  // _(shops).each(function(shop){
-      
-  //     console.log(shop.id);
-    
-  //     var latitude = $("#" + shop.id).data("latitude");
-  //     var longitude = $("#" + shop.id).data("longitude");
-  //     addMarker(latitude, longitude);
-
-  // });
-
-// one approach is to initiate a JSON request 
-// and make the Rails controller respond to JSON
-
-// hardcoded markers
-// addMarker(51.516614, -0.079358);
-// addMarker(55.957175, -3.187894);
-// addMarker(51.521371, -0.210583);
-// addMarker(51.490694, -0.153452);
-// addMarker(51.528913, -0.077664);
 
 });
 
@@ -47,5 +26,13 @@ $(document).ready(function() {
       position: new google.maps.LatLng(lat, lng),
       map: map
     });
+
+    google.maps.event.addListener(marker, 'click', function() {
+      map.setZoom(16);
+      map.setCenter(marker.getPosition());
+    });
+
   }
+
+    
 
