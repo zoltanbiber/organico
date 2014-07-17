4bdb43a6801eeb9ff53526cf84636af990a65d2d
Zoltan Biber <zoltan.biber@gmail.com>
2014-07-17 12:27:13 +0100

    added and removed some comments

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,3 @@
-# require 'rails_helper'
-
-# RSpec.describe User, :type => :model do
-
-#   it { should validate_uniqueness_of(:username) }
-#   it { should validate_presence_of(:username) }
-#   it { should validate_presence_of(:company_name) }
-
-# end
-
 require 'rails_helper'
 
 describe User do
